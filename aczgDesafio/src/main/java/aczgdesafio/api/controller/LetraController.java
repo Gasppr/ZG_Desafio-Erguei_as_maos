74af07b3d9c782c7987ac0aaf8edd5b843bb6bab
Paulo Roberto <paulorantunes13@hotmail.com>
2024-12-04 20:20:26 -0300

    feat: Criação inicial do controller de entrega da letra

--- aczgDesafio/src/main/java/aczgdesafio/api/controller/LetraController.java
+++ aczgDesafio/src/main/java/aczgdesafio/api/controller/LetraController.java
@@ -1,15 +1,16 @@
 package aczgdesafio.api.controller;
 
+import aczgdesafio.api.Letra;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/letra")
-public class letraController {
+public class LetraController {
 
     @GetMapping
     public String letra(){
-        return "Hello world!";
+        Letra letra = new Letra();
+
+        return letra.getLetra();
     }
 }
